914ffdfa1f9e6029ac030cb433ac67439df87077
LouPit929 <louispitingolo@gmail.com>
2020-11-16 00:33:16 -0500

    Update themepark-schema.sql

--- themepark-schema.sql
+++ themepark-schema.sql
@@ -14,55 +14,55 @@ DROP TABLE ACTIVITYLOG CASCADE CONSTRAINTS;
 DROP TABLE WORKS_ON CASCADE CONSTRAINTS;
 
 CREATE TABLE PARK_ZONE(
-    ZoneID      Int        NOT NULL,
-    ZoneName    VarChar(50)     NOT NULL,
-    OpeningDate     Date    NULL,
+    ZoneID         Int           NOT NULL,
+    ZoneName       VarChar(50)   NOT NULL,
+    OpeningDate    Date          NULL,
     CONSTRAINT       ZONE_PK     PRIMARY KEY(ZoneID)
 );
 
 CREATE TABLE ATTRACTION(
-    AttractionID    Int     NOT NULL,
-    AttractionName  VarChar(50)     NOT NULL,
-    AttractionType   VarChar(50)      NOT NULL, -- ride, restaurant, show, waterpark, etc.
-    DateOpened      Date    NOT NULL,
-    CurrStatus      VarChar(15)     NOT NULL, --active, inactive, undermaintenance
-    ZoneID      Int     NOT NULL,
-    CONSTRAINT       ATTRACTION_PK     PRIMARY KEY(AttractionID),
+    AttractionID     Int             NOT NULL,
+    AttractionName   VarChar(50)     NOT NULL,
+    AttractionType   VarChar(50)     NOT NULL, -- ride, restaurant, show, waterpark, etc.
+    DateOpened       Date            NOT NULL,
+    CurrStatus       VarChar(15)     NOT NULL, --active, inactive, undermaintenance
+    ZoneID           Int             NOT NULL,
+    CONSTRAINT       ATTRACTION_PK      PRIMARY KEY(AttractionID),
     FOREIGN KEY (ZoneID) REFERENCES Park_Zone(ZoneID)
     );
     
 CREATE TABLE EMPLOYEE(
-    EmployeeID      Int     NOT NULL,
-    FirstName    VarChar(20)     NOT NULL,
-    MiddelInit      Char(1)     NULL,
+    EmployeeID      Int             NOT NULL,
+    FirstName       VarChar(20)     NOT NULL,
+    MiddelInit      Char(1)         NULL,
     LastName        VarChar(20)     NOT NULL,
     JobTitle        VarChar(50)     NOT NULL, 
     Birthdate       Date            NOT NULL,
     Address         VarChar(50)     NULL,
-    Salary          Int         NOT NULL,
+    Salary          Int             NOT NULL,
     CONSTRAINT EMPLOYEE_PK PRIMARY KEY(EmployeeID)
     );
     
 CREATE TABLE CUSTOMER(
-    CustomerID      Int         NOT NULL,
-    Fname       VarChar(20)     NOT NULL,
-    Lname       VarChar(20)     NOT NULL,
-    TicketType      VarChar(20)   NOT NULL,
+    CustomerID      Int             NOT NULL,
+    Fname           VarChar(20)     NOT NULL,
+    Lname           VarChar(20)     NOT NULL,
+    TicketType      VarChar(20)     NOT NULL,
     CONSTRAINT CUSTOMER_PK PRIMARY KEY(CustomerID)
     );
     
 CREATE TABLE ActivityLog(
-    CustomerID      Int     NOT NULL,
-    AttractionID    Int     NOT NULL,
-    AtrTime         TIMESTAMP    NOT NULL,
+    CustomerID      Int         NOT NULL,
+    AttractionID    Int         NOT NULL,
+    AtrTime         DATE        NOT NULL,
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
     FOREIGN KEY (AttractionID) REFERENCES Attraction(AttractionID)
-);
+    );
 
 CREATE TABLE Works_On(
-    EmployeeID  Int     NOT NULL,
+    EmployeeID      Int     NOT NULL,
     AttractionID    Int     NOT NULL,
-    ShiftHours     Int     NOT NULL,
+    ShiftHours      Int     NOT NULL,
     FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID),
     FOREIGN KEY (AttractionID) REFERENCES Attraction(AttractionID)
     );
